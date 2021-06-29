7295398458cf18e2071d309cb4a74e3599d074d3
Lucas Avila <lucas_avila@div-it.com.ar>
2021-06-29 20:49:21 -0300

    cambios de la charla

--- src/app/pages/factura/factura.page.ts
+++ src/app/pages/factura/factura.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Pedido } from '../../entities/pedido';
+import { User } from '../../entities/user';
+import { LoginService } from '../../services/login.service';
+import { UsersService } from '../../services/users.service';
+import { MenuService } from '../../services/menu.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-factura',
@@ -7,7 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacturaPage implements OnInit {
 
-  constructor() { }
+  
+
+  usuarioLogueado:User = new User();
+  pedido:Pedido = new Pedido();
+
+  constructor(    
+    public loginService:LoginService,
+    public userService:UsersService,
+    public productoSvc:MenuService,
+    public router:Router,
+  ) {
+    
+    this.loginService.usuarioLogueado.then(usr=>{
+      this.usuarioLogueado = usr;            
+      this.productoSvc.getAllPedidos().subscribe(pedidos=>{
+        pedidos.forEach(pedido=>{
+          if(pedido.uidCliente == this.usuarioLogueado.uid){
+            this.productoSvc.getPedidoByUId(pedido.uid).then(pedido => {         
+               if(!pedido){
+                 this.router.navigateByUrl('dashboard/pagina-espera-elaboracion');
+               }else{
+                 this.pedido = pedido;
+               }
+            });       
+
+          }
+        })
+      })
+    });
+  }
 
   ngOnInit() {
   }
