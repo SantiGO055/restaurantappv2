ed4756624f3a42be12a493513489b019a6c76418
Lucas Avila <lucas_avila@div-it.com.ar>
2021-06-29 20:01:56 -0300

    agregando qr dni

--- src/app/entities/DNIQRResponse.ts
+++ src/app/entities/DNIQRResponse.ts
@@ -1,7 +1,6 @@
       //00232638557@MALAGAMBA@MARIA MONICA@F@27462786@B@12/11/1979@02/12/2013
       //00490436790@TRUCCO@CONSTANZA ZO@F@42528721@A@13/04/2000@20/04/2017@234
-      //00204605209@DOMINGUEZ@DANTE ROGELIO@M@37898702@C@05/04/1994@15/07/2013      
-      
+      //00204605209@DOMINGUEZ@DANTE ROGELIO@M@37898702@C@05/04/1994@15/07/2013            
 export class DNIQRResponse{    
     tramite:string    
     apellido:string 
@@ -13,16 +12,34 @@ export class DNIQRResponse{
     emision:string    
     
     static fromQR(codigo:string){
-        var splitted = codigo.split("@"); 
-        return {
-           tramite:splitted[0],
-           apellido:splitted[1],
-           nombre:splitted[2],
-           sexo:splitted[3],
-           numero:splitted[4],
-           ejemplar:splitted[5],
-           fechaNacimiento:splitted[6],
-           emision:splitted[7],           
-        } as DNIQRResponse;
+        var splitted = codigo.trim().split("@").filter(part => part.length > 0 ); 
+        if(splitted[0] == 'A' ||  splitted[0] == 'B'||  splitted[0] == 'C'||  splitted[0] == 'D'){
+            //dni libreta
+            return {
+                tramite:splitted[8],
+                apellido:splitted[2],
+                nombre:splitted[3],
+                sexo:splitted[6],
+                numero:'',
+                ejemplar:splitted[0],
+                fechaNacimiento:splitted[5],
+                emision:splitted[7],           
+             } as DNIQRResponse;
+           }else{
+               //dni tarjeta
+            return {
+                tramite:splitted[0],
+                apellido:splitted[1],
+                nombre:splitted[2],
+                sexo:splitted[3],
+                numero:splitted[4],
+                ejemplar:splitted[5],
+                fechaNacimiento:splitted[6],
+                emision:splitted[7],           
+             } as DNIQRResponse;
+           }
+
+
+        
     }
 }
--- src/app/services/lectorqrdni.service.ts
+++ src/app/services/lectorqrdni.service.ts
@@ -24,9 +24,6 @@ export class LectorQrDniService  extends LectorqrService{
   async escanear():Promise<DNIQRResponse>{
     try{
       const codigo = await super.scan();      
-      //const  codigo  ='00232638557@MALAGAMBA@MARIA MONICA@F@27462786@B@12/11/1979@02/12/2013';
-      //const  codigo  ='00490436790@TRUCCO@CONSTANZA ZO@F@42528721@A@13/04/2000@20/04/2017@234';
-      //const  codigo  ='00204605209@DOMINGUEZ@DANTE ROGELIO@M@37898702@C@05/04/1994@15/07/2013';
       return DNIQRResponse.fromQR(codigo);        
     }catch( error ){
       throw new SysError('Codigo QR invalido')
