f6b478c1fbfacf7d04a0df166cf41805dd44b6cc
Lucas Avila <lucas_avila@div-it.com.ar>
2021-07-11 19:02:31 -0300

    eliminando usuario de mas

--- src/seed/usuarios.ts
+++ src/seed/usuarios.ts
@@ -5,10 +5,6 @@ export const USUARIOS_TEST :LoginTestData[]= [
     "username":"avillucas+duenio1@gmail.com",
     "password":"1q2w3e4r5t6y"    
   },
-  {        
-    "username":"avillucas+supervisor1@gmail.com",
-    "password":"1q2w3e4r5t6y"    
-  },
   {
     "username":"avillucas+maitre1@gmail.com",
     "password":"1q2w3e4r5t6y",   
@@ -18,27 +14,15 @@ export const USUARIOS_TEST :LoginTestData[]= [
     "password":"1q2w3e4r5t6y",   
   },
   {
-    "username":"avillucas+bartender1@gmail.com",
-    "password":"1q2w3e4r5t6y",   
-  },
-  {
     "username":"avillucas+mozo1@gmail.com",
     "password":"1q2w3e4r5t6y"    
   },
   {
     "username":"avillucas+manycliente@gmail.com",
     "password":"1q2w3e4r5t6y"    
-  },  
+  },      
   {
     "username":"avillucas+rechazado@gmail.com",
     "password":"1q2w3e4r5t6y"    
-  },  
-  {
-    "username":"avillucas+rechazado@gmail.com",
-    "password":"1q2w3e4r5t6y"    
-  }, 
-  {
-    "username":"j0bbkhdlr6@coffeetimer24.com",
-    "password":"123456"    
   }, 
 ];
