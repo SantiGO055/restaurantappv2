90e792ca206f53c927165df3d4777fa0fdd3abf5
Lucas Avila <lucas_avila@div-it.com.ar>
2021-07-11 21:22:40 -0300

    eliminando el + en los usuarios de test

--- src/app/pipes/user-name.pipe.ts
+++ src/app/pipes/user-name.pipe.ts
@@ -5,16 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UserNamePipe implements PipeTransform {
 
-  readonly PRINCIPIO = 'avillucas';
+  readonly PRINCIPIO = 'avillucas\\+';
   readonly FIN = '@gmail.com';
   
  
   transform(value: any): string {    
     if (!value) {
       return value;
-    }
+    }    
     return value
-      .replace(new RegExp(this.PRINCIPIO, 'g'), '')
+      .replace(new RegExp(this.PRINCIPIO, 'g'), '')      
       .replace(new RegExp(this.FIN, 'g'), '');
   }
 
